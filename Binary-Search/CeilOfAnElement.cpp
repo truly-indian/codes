22319a3cdb3132244bf7b681a0bd599393ec3e23
Malik99-rapido <deepak.malik@rapido.bike>
2021-07-11 17:45:55 +0530

    binary | search

---
+++ Binary-Search/CeilOfAnElement.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Program to find ceil of an element.
+
+    int arr[] = {1, 2, 2, 4, 5};
+    int s = 0;
+    int e = 4;
+    int el = 5;
+    int ans = -1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (arr[mid ] == el) {
+           ans = arr[mid];
+           break;
+        }else if(arr[mid] > el) {
+            ans = arr[mid];
+            e = mid - 1;
+        }else {
+            s = mid + 1;
+        }
+        
+    }
+cout<<ans<<endl;
+    return 0;
+}
---
+++ Binary-Search/FloorOfAnElement.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ // program to find floor of an element.
+
+    int arr[] = {1, 2, 3, 4, 8};
+    int s = 0;
+    int e = 4;
+    int el = 10;
+    int ans = -1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == el)
+        {
+            ans = arr[mid];
+            break;
+        }
+        else if (arr[mid] > el)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            ans = arr[mid];
+            s = mid + 1;
+        }
+    }
+    cout << ans << endl;
+}
